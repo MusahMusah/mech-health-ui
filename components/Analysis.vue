07ca771fd352829687b99962e1342789169af646
MusahMusah <shehialiyu0@gmail.com>
2021-07-29 10:19:18 +0100

    Added navigation and Analysis componenets

---
+++ components/Analysis.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="analysis-wrapper">
+    <h4 class="text-center md">Today</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Analysis'
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Navbar />
+    <Analysis />
   </div>
 </template>
 
